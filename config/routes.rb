ccdcfcbe2524f6cda8a5528edd9c3673c12623dd
rachelkathleen <rachel.hawa@gmail.com>
2019-11-27 18:42:31 -0600

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'sessions#welcome'
+  
+  get '/signup' => 'users#new'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
+  get '/dashboard' => 'users#dashboard'
+
+  #routes for omniauth
+  get '/auth/:provider/callback' => 'sessions#omniauth'
+
+
+  get '*path' => redirect('/')
+
 end
