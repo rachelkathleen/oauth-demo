8ec57984352dd76f12233d2ef79cbf7aa31ca5f3
rachelkathleen <rachel.hawa@gmail.com>
2019-11-27 18:10:47 -0600

    create with validations and omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
-end
+    validates :user_name, :email, presence: true
+    validates :user_name, :email, uniqueness: true
+    has_secure_password  #password validations
+  
+    def self.from_omniauth(auth)
+      #Creates a new user only if it doesn't exist
+      where(email: auth.info.email).first_or_initialize do |user|
+        user.first_name = auth.info.name
+        user.email = auth.info.email
+        if auth.info.uid
+          user.uid = auth.info.uid
+        end
+        user.password = SecureRandom.hex
+      end
+    end
+  end
