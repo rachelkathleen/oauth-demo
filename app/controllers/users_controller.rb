33beaed50f14ce131e886bb655286b4a2f72a791
rachelkathleen <rachel.hawa@gmail.com>
2019-11-27 18:10:28 -0600

    create with crud actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
-end
+    skip_before_action :require_login, only: [:new, :create]
+  
+    def new
+      @user = User.new
+    end
+  
+    def create
+      # normal create action
+      @user = User.create(user_params)
+      return redirect_to controller: 'users', action: 'new' unless @user.save
+      session[:user_id] = @user.id
+      redirect_to dashboard_path
+    end
+  
+    def home
+      @user = current_user
+      unless logged_in?
+        redirect_to dashboard_path
+      end
+    end  
+  
+    private
+  
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+  
+  end
